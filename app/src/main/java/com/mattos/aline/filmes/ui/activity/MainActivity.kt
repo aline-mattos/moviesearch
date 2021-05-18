4a7fe2c8ed7ed9ac2bdfcd8a6e7ffea65ccc2f21
Aline <abelomo95@gmail.com>
2021-05-18 13:28:55 +0100

    Reorganizing Archives

---
+++ app/src/main/java/com/mattos/aline/filmes/ui/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.mattos.aline.filmes.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mattos.aline.filmes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //Setup
+    private fun setup() {
+
+    }
+}
