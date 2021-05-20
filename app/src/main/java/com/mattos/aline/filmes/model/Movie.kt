57dcee0ca4ad0bf188385e1bacf4b202117e7837
Aline <abelomo95@gmail.com>
2021-05-20 16:38:22 +0100

    MovieClass att

--- app/src/main/java/com/mattos/aline/filmes/model/Movie.kt
+++ app/src/main/java/com/mattos/aline/filmes/model/Movie.kt
@@ -1,12 +1,16 @@
 package com.mattos.aline.filmes.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
+@Entity
 data class Movie (
+    @PrimaryKey(autoGenerate = false)
     @SerializedName("id") var id: Long? = null,
-    @SerializedName("title") var title: String? = null,
-    @SerializedName("tagline") var tagline: String? = null,
-    @SerializedName("genre") var genre: String? = null
+    @ColumnInfo(name = "title")
+    @SerializedName("title") var title: String? = null
 ) : Serializable {
 }
